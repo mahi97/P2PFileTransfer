9f3445eba89df65944676b884fc33610936295d8
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2018-12-22 22:30:42 +0330

    fix prtotype

--- src/ir/aut/ftp2p/core/Receiver.java
+++ src/ir/aut/ftp2p/core/Receiver.java
@@ -4,4 +4,13 @@ package ir.aut.ftp2p.core;
  * Created by Rahimi on 12/22/18.
  */
 public class Receiver {
+
+    public Receiver() {
+
+    }
+
+    public void receive(String file) {
+
+    }
+
 }
--- src/ir/aut/ftp2p/core/Transfer.java
+++ src/ir/aut/ftp2p/core/Transfer.java
@@ -4,4 +4,13 @@ package ir.aut.ftp2p.core;
  * Created by Rahimi on 12/22/18.
  */
 public class Transfer {
+
+    public Transfer() {
+
+    }
+
+    public void send(String file, String path) {
+
+    }
+
 }
